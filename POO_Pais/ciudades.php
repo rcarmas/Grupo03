dcbfea2aed7f5150426f67fb8b9a705fc16edee5
jhonnyNavarro <jnnavarro@espe.edu.ec>
2023-02-28 00:11:36 -0500

    .

--- POO_Pais/ciudades.php
+++ POO_Pais/ciudades.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ciudades</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
@@ -17,12 +18,18 @@
 
 	echo '<a href="./index.php">Regresar</a>';
 
-	$html = '<br><br><center><table border=1 alingn="center" action=""><tr>';
+	$html = '<center>
+	<div class="container">
+	<div class="col-lg-6">
+	<br><br>
+	<img src="./img/bandera' . $confederacion . '.png" width="300px">
+	<br><br>
+	<br><br><table border=1 alingn="center" action="" class="table table-striped table-bordered border border-secondary">';
 
 	foreach ($ciudades as $pais => $ciudad)
 		if ($pais == $confederacion) {
 			// TITULO DE LA TABLA
-			$html .= ' <tr><th colspan=" ' . count($ciudad) . '" bgcolor="#EC7063"> ' . $pais . '</th></tr><tr>';
+			$html .= ' <tr><th colspan=" ' . count($ciudad) . '" bgcolor="#EC7063" class="text-center bg-success text-white"> ' . $pais . '</th></tr><tr>';
 
 			foreach ($ciudad as  $indice => $data)
 				$html .= "<th> $indice </th>";
@@ -38,7 +45,7 @@
 			}
 		}
 
-	$html .= "</table></center>";
+	$html .= "</table></div></div></center>";
 
 	echo $html;
 	?>
